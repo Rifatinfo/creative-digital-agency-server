c6f975f69b0b351f7c8d49c2b7d94e587a3dbc79
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-12-20 13:08:43 +0600

    add service-plan

--- src/app/modules/servicePlan/servicePlan.controller.ts
+++ src/app/modules/servicePlan/servicePlan.controller.ts
@@ -1,11 +1,31 @@
 import { Request, Response } from "express";
 import { catchAsync } from "../../middlewares/catchAsync";
+import { ServicePlanService } from "./servicePlan.service";
+import { sendResponse } from "../../middlewares/sendResponse";
+import { StatusCodes } from "http-status-codes";
 
-const createServicePlan = catchAsync(async (  req : Request, res : Response) => {
-
+const createServicePlan = catchAsync(async (req: Request, res: Response) => {
+    const { serviceId, plans } = req.body;
+    const result = await ServicePlanService.createPlanIntoDB(serviceId, plans);
+    
+    sendResponse(res, {
+        statusCode: StatusCodes.CREATED,
+        success: true,
+        message: "Service plan Successfully",
+        data: result
+    });
 });
-const getServicePlan = catchAsync(async (  req : Request, res : Response) => {
 
+const getServicePlan = catchAsync(async (req: Request, res: Response) => {
+    const { serviceId } = req.params;
+    const result = await ServicePlanService.getPlansByServiceFromDB(serviceId);
+   
+    sendResponse(res, {
+        statusCode: StatusCodes.OK,
+        success: true,
+        message: "Plans fetched Successfully",
+        data: result
+    });
 });
 
 export const ServicePlanController = {
--- src/app/modules/servicePlan/servicePlan.service.ts
+++ src/app/modules/servicePlan/servicePlan.service.ts
@@ -1,12 +1,30 @@
+import { prisma } from "../../config/db"
 import { IServicePlan } from "./servicePlan.interface"
 
-const createPlanIntoDB = async (payload: IServicePlan) => {
+const createPlanIntoDB = async (serviceId: string,
+    plans: IServicePlan[]) => {
+    return prisma.servicePlan.createMany({
+        data: plans.map(plan => ({
+            name: plan.name,
+            price: plan.price,
+            period: plan.period,
+            description: plan.description,
+            ctaText: plan.ctaText,
+            highlighted: plan.highlighted ?? false,
+            features: plan.features,
 
-}
+            currency: "BDT",
+            // stripePriceId: "", // Set to an empty string instead of null
+            serviceId,
+        })),
+    });
+};
 
-const getPlansByServiceFromDB = async () => {
-
-}
+const getPlansByServiceFromDB = async (serviceId: string) => {
+    return prisma.servicePlan.findMany({
+        where: { serviceId }
+    });
+};
 
 export const ServicePlanService = {
     createPlanIntoDB,
