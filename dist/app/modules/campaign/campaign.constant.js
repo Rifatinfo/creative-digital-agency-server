ff615bb3bf6cba37141613a4d2827945987289fa
Rifatinfo <mdrifathossainsinfo@gmail.com>
2026-01-05 19:46:16 +0600

    add vercel

--- dist/app/modules/campaign/campaign.constant.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userFilterableFields = exports.userSearchField = void 0;
-exports.userSearchField = ["title", "category", "subcategory", "brand"];
-exports.userFilterableFields = ["views", "dateLabel", "searchTerm", "duration", "featured"];
--- dist/app/modules/service/service.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ServiceRoutes = void 0;
-const express_1 = require("express");
-const service_controller_1 = require("./service.controller");
-const router = (0, express_1.Router)();
-router.post("/create-service", service_controller_1.ServiceController.createService);
-router.get("/", service_controller_1.ServiceController.getService);
-exports.ServiceRoutes = router;
